273376e49822413db05adec3f99e542dbb4f016a
Mauricio Rosales <mrosales.correouniversitario@gmail.com>
2026-09-20 12:36:41 -0300

    Simplificar DTO con Lombok y eliminar código anterior

--- src/main/java/com/example/Ejercicio1/DTO/NumerosDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.Ejercicio1.DTO;
-
-import lombok.Data;
-
-@Data
-public class NumerosDTO {
-    private int numero1;
-    private int numero2;
-}
--- src/main/java/com/example/Ejercicio1/DTO/SolicitudAsistenciaDTO.java
+++ src/main/java/com/example/Ejercicio1/DTO/SolicitudAsistenciaDTO.java
@@ -1,56 +1,18 @@
 package com.example.Ejercicio1.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SolicitudAsistenciaDTO {
 
     private int obligaciones;
     private int asistencias;
     private int abonos;
     private double porcentajeMinimo;
-
-    public SolicitudAsistenciaDTO() {
-    }
-
-    public SolicitudAsistenciaDTO(
-            int obligaciones,
-            int asistencias,
-            int abonos,
-            double porcentajeMinimo) {
-
-        this.obligaciones = obligaciones;
-        this.asistencias = asistencias;
-        this.abonos = abonos;
-        this.porcentajeMinimo = porcentajeMinimo;
-    }
-
-    public int getObligaciones() {
-        return obligaciones;
-    }
-
-    public void setObligaciones(int obligaciones) {
-        this.obligaciones = obligaciones;
-    }
-
-    public int getAsistencias() {
-        return asistencias;
-    }
-
-    public void setAsistencias(int asistencias) {
-        this.asistencias = asistencias;
-    }
-
-    public int getAbonos() {
-        return abonos;
-    }
-
-    public void setAbonos(int abonos) {
-        this.abonos = abonos;
-    }
-
-    public double getPorcentajeMinimo() {
-        return porcentajeMinimo;
-    }
-
-    public void setPorcentajeMinimo(double porcentajeMinimo) {
-        this.porcentajeMinimo = porcentajeMinimo;
-    }
 }
